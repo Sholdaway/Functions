8ddc37b8c5e4781508afde39da6baafb3ee1bb4e
Sholdaway <Sam.holdaway@gmail.com>
2017-03-24 15:46:07 +0000

    Adding the first of the SQL functions

---
+++ Build-SQLCommandText.ps1
@@ -0,0 +1,32 @@
+﻿function BuildSQLInsertCommandText {
+
+    #This function uses the VMQ output object to create the values string used in the SQL insert statement
+    #It takes an input object and converts it to a string
+    #This isn't generic and so it's rubbish. Need to find a way to build the SQL Command object without caring about the input object
+    #Should be able to make this cater for insert/update and maybe delete?
+    
+    [CmdletBinding()]
+    Param 
+    (
+        $InputObject
+    )
+    
+    $SQLCommand = @()
+
+    Write-Verbose "Starting the for loop to create the SQL values"
+    $InputObject | ForEach-Object {
+        $Properties = @{'CommandText' = "'$($_.ComputerName)', `
+                                         '$($_.AdapterName)', `
+                                         '$($_.AdapterStatus)', `
+                                         '$($_.VMQRegSetting)', `
+                                         '$($_.VMQEnabledDriver)', `
+                                         '$($_.VMQEnabledOS)', `
+                                         '$($_.VMQNumReceiveQueues)', `
+                                         '$($_.VMQMaxProcessors)'"}
+        $SQLCommand += New-Object -TypeName PSObject -Property $Properties
+    }
+
+    $SQLCommand
+}
+
+$CommandToInsertIntoDB = BuildSQLInsertCommandText -InputObject $JobResults -Verbose
---
+++ Create-SQLConnection.ps1
@@ -0,0 +1,18 @@
+﻿#This simply generates a SQL connection object and assigns it a connection string. No connections to the DB are opened here
+
+function CreateSQLConnection {
+    [CmdletBinding()]
+    Param 
+    (
+        [string]$SQLServer,
+        [string]$Instance,
+        [string]$DatabaseName
+    )
+    
+    Write-Verbose "Creating (but not opening!) a connection to $DatabaseName on $SQLServer\$Instance"
+    $Conn = New-Object System.Data.SqlClient.SqlConnection
+    $Conn.ConnectionString = "Data Source=$SQLServer\$Instance; Initial Catalog=$DatabaseName; Integrated Security=SSPI"
+    $Conn
+}
+
+$Conn = CreateSQLConnection -SQLServer server01 -Instance instance01 -DatabaseName testdb -Verbose
