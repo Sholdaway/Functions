ffd02b7d5391fea3d819b82c2cd1c9a844c2e817
Sholdaway <Sam.holdaway@gmail.com>
2017-03-24 15:40:32 +0000

    First draft of job handling functions

---
+++ Invoke-FunctionAsJob.ps1
@@ -0,0 +1,46 @@
+﻿function InvokeFunctionAsJobThrottled {
+    #Maybe think about making the max threads parameter optional? Or upping the default thread count?
+
+    [CmdletBinding()]
+    Param (
+        [string[]]$Computers,
+        [scriptblock]$FunctionToRun,
+        $MaxThreads = 10
+    )
+
+    Write-Verbose ('Running function defined in $FunctionToRun, with a maximum of ' + "$MaxThreads" + ' job threads')
+
+    $Computers | ForEach-Object {
+
+        While (@(Get-Job | Where-Object {$_.state -eq 'Running'}).Count -ge $MaxThreads) 
+        {
+            Write-Verbose "Max jobs reached, maximum threads is $MaxThreads"
+            Start-Sleep -Seconds 3
+        }
+
+        Invoke-Command -ComputerName $_ -ScriptBlock $FunctionToRun -AsJob
+        
+    }
+}
+
+
+function GetProcessorInfo {
+    Get-WmiObject win32_processor
+}
+
+
+$FunctionToRun = 'GetProcessorInfo'
+Invoke-Command -ComputerName testserver01 -ScriptBlock ${Function:$FunctionToRun}
+
+
+${Function:$FunctionToRun}
+
+$HVHosts = "testserver01","testserver02"
+
+InvokeFunctionAsJobThrottled -Computers $HVHosts -FunctionToRun $Function:GetProcessorInfo -MaxThreads 10 -Verbose
+
+
+
+
+Write-Verbose ("$TestNumber" + "$TestWord") -Verbose
+
---
+++ Receive-JobOutputAndRemove.ps1
@@ -0,0 +1,20 @@
+﻿function ReceiveJobOutputAndRemove {
+    [CmdletBinding()]
+    Param ()
+    
+    #Would like to see a begin, process, end construct here with job cleanup in the finally block
+
+    While (@(Get-Job | Where-Object {$_.state -eq 'Running'}).Count -ne 0)
+    {
+        Write-Verbose ('Jobs are still running. Number of jobs still to complete is ' + ((Get-Job | Where-Object {$_.state -eq 'Running'}).Count))
+        Start-Sleep -Seconds 3
+    }
+
+    $JobOutput = Get-Job | Receive-Job | Select-Object * -ExcludeProperty pscomputername,runspaceid
+
+    Get-Job | Remove-Job
+
+    $JobOutput
+}
+
+$JobResults = ReceiveJobOutputAndRemove -Verbose
